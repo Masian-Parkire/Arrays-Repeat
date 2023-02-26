55e9e2e6d0dfca645192b0596a77f91cb964c18c
Masian Parkire <parkiremasian@gmail.com>
2023-02-26 12:27:03 +0300

    made changes in Arrays Repeat

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,35 +1,44 @@
+import kotlin.math.pow
+
 fun main() {
-    takesInNames(arrayOf("Kevin","Ian","Snaida","Tevin"))
-    namesOfCities(arrayOf<String>())
-    takesNumbers(arrayOf())
-    var cereals = takesInNam(arrayOf("Beans","maize","wheat"))
-    println(cereals)
+    takeNames("Kevin","Ian","Snaida","Tevin")
+    namesOfCities()
+    takesNumbers()
+    takesInNam("Cat","Dog","Cow")
+    square(4,7,8)
+    number()
+    roots()
 
 }
-
-fun takesInNames(names:Array<String>) {
+fun takeNames(nam1:String,nam2:String,nam3:String,nam4:String){
+    var names = arrayOf(nam1,nam2,nam3,nam4)
     println(names.contentToString())
+}
 
-}
-fun namesOfCities(cities:Array<String>){
+fun namesOfCities(){
     var cities = arrayOf("harare", "mumbai", "dodoma", "jakarta")
-    cities.forEach { names ->
-        println(names.capitalize())
+    println(cities.map{it.capitalize()}.sorted())
     }
-}
-fun takesNumbers(numbers:Array<Int>){
+fun takesNumbers(){
     var numbers = arrayOf(32, 17,4,213,78,43,90,31,3,73,11,158,62)
-    var sum = numbers[1].plus(numbers[4])
-    println(sum)
+    println(numbers[1]+numbers[4])
     var index = numbers.indexOf(158)
     println(index)
-    var sortedNumbers = numbers.sortedArray()
-    println(sortedNumbers.contentToString())
+    println(numbers.sortedArray().contentToString())
+}
+fun takesInNam(tag1:String,tag2:String,tag3:String):String{
+    var identity = arrayOf(tag1,tag2,tag3)
+    return identity.contentToString()
+}
+fun square(numb1:Int,numb2:Int,numb3:Int){
+    var sqr = arrayOf(numb1*numb1,numb2*numb2,numb3*numb3)
+    println(sqr.contentToString())
 
 }
-fun takesInNam(nam: Array<String>):String{
-    var nam = nam.contentToString()
-    return nam
-
-
+fun number(){
+    var num = arrayOf(3,5,6,7)
+    for (nm in num){
+        println(nm.toDouble().pow(2))
+    }
 }
+
